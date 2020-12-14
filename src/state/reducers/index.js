380d8f76a023776e4355eaefa9c8c1a07ac52030
yogeshepam <53252472+yogeshepam@users.noreply.github.com>
2020-12-14 15:27:48 +0200

    Added material-ui, (#3)
    Added react router dom,
    Added 404 for no matching route,
    Added Homepage,
    Removed unnecessary code from template.

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,3 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import counterReducer from '../../features/counter/counterSlice';
 
-export default combineReducers({
-  counter: counterReducer,
-});
+export default combineReducers({});
