941c40c295486b6f23cf392bae6e7bb07fcb43f5
Yogesh Devgun <yogesh_devgun@epam.com>
2020-12-15 15:47:13 +0200

    Updated snapshot test

--- src/views/EpisodePage/EpisodePage.test.js
+++ src/views/EpisodePage/EpisodePage.test.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { initialState as episodeInitalState } from './episodeSlice';
 import EpisodePage from './EpisodePage';
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useParams: () => ({
-    id: '1234',
+    episodeId: '1234',
   }),
+}));
+jest.mock('react-redux', () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
 }));
 
 describe('views', () => {
   describe('EpisodePage', () => {
     describe('UI', () => {
       it('should match the snapshot', () => {
+        useSelector.mockImplementation((selector) => selector({ episode: episodeInitalState }));
         const wrapper = shallow(<EpisodePage />);
 
         expect(wrapper).toMatchSnapshot();
